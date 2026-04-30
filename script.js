866cb7d03fb531042ee9e4ea351333d5eff69827
AmayaGM <amayagalaviz@outlook.com>
2026-04-29 21:53:29 -0600

    modal 1

--- script.js
+++ script.js
@@ -4,18 +4,17 @@ function expandCard(card) {
     if (!card.classList.contains('active')) {
         card.classList.add('active');
         overlay.classList.add('show');
+        document.body.style.overflow = 'hidden'; // Evita que se mueva el fondo
     }
 }
 
 function closeCard(event, button) {
-    // Evita que el clic en la X también dispare el clic en la tarjeta
     event.stopPropagation();
-    
     const card = button.parentElement;
     card.classList.remove('active');
     overlay.classList.remove('show');
+    document.body.style.overflow = 'auto'; // Devuelve el scroll al cerrar
 }
-
 // Cerrar al hacer clic en el fondo oscuro
 overlay.addEventListener('click', () => {
     const activeCard = document.querySelector('.card.active');
